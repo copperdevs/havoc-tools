3804c4b4ada5fce02dd4c918dbeec4232d42bd1f
copper <79734708+OlympianGames@users.noreply.github.com>
2023-12-10 23:05:22 -0600

    game switcher

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
     multiply();
+    showContent("gameselector-valorant");
 
     document.getElementById('havocdpi').addEventListener('input', function () {
         document.getElementById('havocdpivalue').innerHTML = this.value;
@@ -44,9 +45,18 @@ function multiply() {
     // results
     var valtohavocsens = (valorantsens * 0.65 * 3.182) * (valorantdpi / havocdpi);
     // document.getElementById('valorantresult').innerHTML = 'Valorant to Havoc sens - ' + Math.round(valtohavocsens * 100)/100;
-    document.getElementById('valorantresult').innerHTML = 'Havoc sens - ' + Math.round(valtohavocsens * 100)/100;
+    document.getElementById('valorantresult').innerHTML = 'Havoc sens - ' + Math.round(valtohavocsens * 100) / 100;
 
     var cs2tohavocsens = (0.41 * cs2sens) * (cs2dpi / havocdpi);
     // document.getElementById('cs2result').innerHTML = 'Counter Strike 2 to Havoc sens - ' + Math.round(cs2tohavocsens * 100)/100;
-    document.getElementById('cs2result').innerHTML = 'Havoc sens - ' + Math.round(cs2tohavocsens * 100)/100;
+    document.getElementById('cs2result').innerHTML = 'Havoc sens - ' + Math.round(cs2tohavocsens * 100) / 100;
 }
+
+function showContent(selectedValue) {
+    // Hide all content elements
+    document.getElementById('gameselector-valorant').style.display = 'none';
+    document.getElementById('gameselector-cs2').style.display = 'none';
+
+    // Show the selected content based on the dropdown value
+    document.getElementById(selectedValue).style.display = 'block';
+  }
