3daf53ffbc3a5e1ac792a788680e30f590123dd6
copper <79734708+OlympianGames@users.noreply.github.com>
2023-12-12 00:20:29 -0600

    yummy scale

--- skin-database/images/ak/generator.py
+++ skin-database/images/ak/generator.py
@@ -4,6 +4,6 @@ with open("skin-database/images/ak/result.html", 'w') as output_file:
     for filename in os.listdir("skin-database/images/ak"):
         print(filename)
         if filename.endswith(".png"):
-            output_file.write(r'<img src="./images/ak/' + filename + r'" alt="Acidvat" width="128" height="128" style="background-color: lightgray;">' + '\n')
+            output_file.write(r'<img src="./images/ak/' + filename + r'" alt="' + filename + r'" width="16%" height="16%" style="background-color: lightgray;">' + '\n')
 
     
