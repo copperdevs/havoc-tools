544e9b3f62ed4a988b1ca355edeef2c1bc9272b4
copperdev <79734708+OlympianGames@users.noreply.github.com>
2023-12-13 15:46:36 -0600

    fixed some local storage errors + formatting

--- sensitivity/script.js
+++ sensitivity/script.js
@@ -1,34 +1,47 @@
-const GameSelectorKey = 'havoctools-gameselector';
-const UnitSelectorKey = 'havoctools-unitselector';
-const ThemeSelectorKey = 'havoctools-themeselector';
-
+const GameSelectorKey = "havoctools-gameselector";
+const UnitSelectorKey = "havoctools-unitselector";
+const ThemeSelectorKey = "havoctools-themeselector";
 
 const Themes = new Map();
-Themes.set('themeselector-light', 'body-light');
-Themes.set('themeselector-dark', 'body-dark');
-Themes.set('themeselector-lighter', 'body-lighter');
-Themes.set('themeselector-darker', 'body-darker');
-Themes.set('themeselector-discord', 'body-discord');
+Themes.set("themeselector-light", "body-light");
+Themes.set("themeselector-dark", "body-dark");
+Themes.set("themeselector-lighter", "body-lighter");
+Themes.set("themeselector-darker", "body-darker");
+Themes.set("themeselector-discord", "body-discord");
 
 function selectorLocalStorageHandler(storageKey, selectorId, defaultSelected) {
-    if (localStorage.getItem(storageKey) === null) {
+    var localStorageData = localStorage.getItem(storageKey);
+    var targetValue = null;
 
-        showGameSelectorContent(defaultSelected);
-        document.getElementById(selectorId).value = defaultSelected;
+    if (localStorageData === null || localStorageData === undefined) {
+        targetValue = defaultSelected;
+    } else {
+        targetValue = localStorageData;
     }
-    else {
-        showGameSelectorContent(localStorage.getItem(storageKey));
-        document.getElementById(selectorId).value = localStorage.getItem(storageKey);
-    }
+
+    document.getElementById(selectorId).value = targetValue;
+    localStorage.setItem(storageKey, targetValue);
 }
 
-
-document.addEventListener('DOMContentLoaded', function () {
-    selectorLocalStorageHandler(GameSelectorKey, 'gameselector', 'gameselector-valorant');
-    selectorLocalStorageHandler(UnitSelectorKey, 'unitselector', 'unitselector-inches');
-    selectorLocalStorageHandler(ThemeSelectorKey, 'themeselector', 'themeselector-light');
+document.addEventListener("DOMContentLoaded", function () {
+    selectorLocalStorageHandler(
+        GameSelectorKey,
+        "gameselector",
+        "gameselector-valorant"
+    );
+    selectorLocalStorageHandler(
+        UnitSelectorKey,
+        "unitselector",
+        "unitselector-inches"
+    );
+    selectorLocalStorageHandler(
+        ThemeSelectorKey,
+        "themeselector",
+        "themeselector-dark"
+    );
 
     showGameSelectorContent(localStorage.getItem(GameSelectorKey));
+    showUnitSelectorContent(localStorage.getItem(UnitSelectorKey));
     themeSelector(localStorage.getItem(ThemeSelectorKey));
 
     multiply();
@@ -36,47 +49,49 @@ document.addEventListener('DOMContentLoaded', function () {
 
 function multiply() {
     // havoc
-    var havocdpi = document.getElementById('havocdpi').value;
+    var havocdpi = document.getElementById("havocdpi").value;
 
-    var gameDpi = document.getElementById('gamedpi').value;
-    var gameSens = document.getElementById('gamesens').value;
+    var gameDpi = document.getElementById("gamedpi").value;
+    var gameSens = document.getElementById("gamesens").value;
 
     var sensResult = 0;
     var distanceResult = 0;
 
     // sens math
-    switch (document.getElementById('gameselector').value) {
-        case 'gameselector-valorant':
-            sensResult = (gameSens * 1.44) * (gameDpi / havocdpi);
+    switch (document.getElementById("gameselector").value) {
+        case "gameselector-valorant":
+            sensResult = gameSens * 1.44 * (gameDpi / havocdpi);
             break;
-        case 'gameselector-cs2':
-            sensResult = (0.45 * gameSens) * (gameDpi / havocdpi);
+        case "gameselector-cs2":
+            sensResult = 0.45 * gameSens * (gameDpi / havocdpi);
             break;
         default:
-            sensResult = (gameSens * 1.44) * (gameDpi / havocdpi);
+            sensResult = gameSens * 1.44 * (gameDpi / havocdpi);
             break;
     }
 
     // distance math
-    switch (document.getElementById('unitselector').value) {
-        case 'unitselector-inches':
-            switch (document.getElementById('gameselector').value) {
-                case 'gameselector-valorant':
-                    distanceResult = (13067 / (gameSens * gameDpi)) / 2.54000562223471;
+    switch (document.getElementById("unitselector").value) {
+        case "unitselector-inches":
+            switch (document.getElementById("gameselector").value) {
+                case "gameselector-valorant":
+                    distanceResult =
+                        13067 / (gameSens * gameDpi) / 2.54000562223471;
                     break;
-                case 'gameselector-cs2':
-                    distanceResult = (41560 / (gameSens * gameDpi)) / 2.54000562223471;
+                case "gameselector-cs2":
+                    distanceResult =
+                        41560 / (gameSens * gameDpi) / 2.54000562223471;
                     break;
                 default:
                     break;
             }
             break;
-        case 'unitselector-centimeters':
-            switch (document.getElementById('gameselector').value) {
-                case 'gameselector-valorant':
+        case "unitselector-centimeters":
+            switch (document.getElementById("gameselector").value) {
+                case "gameselector-valorant":
                     distanceResult = 13067 / (gameSens * gameDpi);
                     break;
-                case 'gameselector-cs2':
+                case "gameselector-cs2":
                     distanceResult = 41560 / (gameSens * gameDpi);
                     break;
                 default:
@@ -84,12 +99,14 @@ function multiply() {
             }
             break;
         default:
-            switch (document.getElementById('gameselector').value) {
-                case 'gameselector-valorant':
-                    distanceResult = (13067 / (gameSens * gameDpi)) / 2.54000562223471;
+            switch (document.getElementById("gameselector").value) {
+                case "gameselector-valorant":
+                    distanceResult =
+                        13067 / (gameSens * gameDpi) / 2.54000562223471;
                     break;
-                case 'gameselector-cs2':
-                    distanceResult = (41560 / (gameSens * gameDpi)) / 2.54000562223471;
+                case "gameselector-cs2":
+                    distanceResult =
+                        41560 / (gameSens * gameDpi) / 2.54000562223471;
                     break;
                 default:
                     break;
@@ -98,33 +115,40 @@ function multiply() {
     }
 
     // setting values
-    document.getElementById('sensresult').innerHTML = 'Havoc Sens - ' + sensResult;
+    document.getElementById("sensresult").innerHTML =
+        "Havoc Sens - " + sensResult;
 
-    switch (document.getElementById('unitselector').value) {
-        case 'unitselector-inches':
-            document.getElementById('distantresult').innerHTML = '360° Distance - ' + distanceResult + 'in';
+    switch (document.getElementById("unitselector").value) {
+        case "unitselector-inches":
+            document.getElementById("distantresult").innerHTML =
+                "360° Distance - " + distanceResult + "in";
             break;
-        case 'unitselector-centimeters':
-            document.getElementById('distantresult').innerHTML = '360° Distance - ' + distanceResult + 'cm';
+        case "unitselector-centimeters":
+            document.getElementById("distantresult").innerHTML =
+                "360° Distance - " + distanceResult + "cm";
             break;
         default:
-            document.getElementById('distantresult').innerHTML = '360° Distance - ' + distanceResult + 'in';
+            document.getElementById("distantresult").innerHTML =
+                "360° Distance - " + distanceResult + "in";
             break;
     }
 }
 
 function showGameSelectorContent(selectedValue) {
     switch (selectedValue) {
-        case 'gameselector-valorant':
-            document.getElementById('game-title').innerHTML = 'Valorant';
+        case "gameselector-valorant":
+            document.getElementById("game-title").innerHTML = "Valorant";
             break;
-        case 'gameselector-cs2':
-            document.getElementById('game-title').innerHTML = 'Counter Strike 2';
+        case "gameselector-cs2":
+            document.getElementById("game-title").innerHTML =
+                "Counter Strike 2";
             break;
         default:
-            document.getElementById('game-title').innerHTML = 'Valorant';
+            document.getElementById("game-title").innerHTML = "Valorant";
             break;
     }
+
+    localStorage.setItem(GameSelectorKey, selectedValue);
 
     multiply();
 }
@@ -141,5 +165,5 @@ function themeSelector(selectedValue) {
 
 function showUnitSelectorContent(selectedValue) {
     multiply();
-    localStorage.setItem(ThemeSelectorKey, selectedValue);
+    localStorage.setItem(UnitSelectorKey, selectedValue);
 }
